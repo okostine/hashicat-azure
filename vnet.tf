b1860f8b000e999108b3ce438e4cfa44e5f739e0
Oleg Kostine <okostine@gmail.com>
2021-05-13 20:50:28 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,6 +2,6 @@ module "network" {
   source  = "app.terraform.io/cloudsk8s-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-  resource_group_name
+  resource_group_name = myresourcegroup
 }
 
